0a9237b9ce3c1e517e02cae26dfbeae1292e392b
bnt247560-lab <bnt247560@gmail.com>
2026-07-03 06:45:42 +0300

    Delete src directory

--- src/downloader.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-pub fn download_video(url: &str, output: &str) -> bool {
-    Command::new("yt-dlp")
-        .args([url, "-o", output, "--quiet"])
-        .status()
-        .map(|s| s.success())
-        .unwrap_or(false)
-}
