ab0b05ff2001ef945bd50767a00fbc0201676aed
bnt247560-lab <bnt247560@gmail.com>
2026-07-03 07:22:58 +0300

    Delete src directory

--- src/models/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod user;
-pub mod video;
-pub mod job;
-
-pub use user::User;
-pub use video::{VideoMetadata, VideoProcessingOptions};
-pub use job::{Job, JobStatus};
--- src/utils/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod file;
-pub mod temp;
-pub mod path;
-pub mod ffprobe;
